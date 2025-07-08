728a1ef9adf2b0f6e121620629621d8679c12e49
AlvPulse <Alvandian.pulseware@gmail.com>
2025-07-08 16:39:01 +0330

    correct adding v3 to main

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("V1")
-
+    print("V2")
+    print("V3")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
